c33d6ce4f8254e2a06a30fdd3cb48157dfea58ca
Jyothirmai Thimmaraju <43008285+dhanajyothirmai@users.noreply.github.com>
2020-09-05 20:58:49 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@
 function Form_Validation()
 {
 	var form = document.getElementById('form').value;
-	var Name = document.getElementById('Name');
-	var E-mail = document.getElementById('E-mail');
-	var PhoneNumber = document.getElementById('PhoneNumber');
+	var Name = document.getElementById('name');
+	var Email = document.getElementById('email');
+	var PhoneNumber = document.getElementById('phonenumber');
 
 	var name_validators = /^[^0-9][0-9a-zA-Z]+$/;
 	var email_validators = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
@@ -16,12 +16,12 @@ function Form_Validation()
 		return false;
 	}
 
-	if(!email.value.match(var email_validators)){
+	if(!email.value.match(email_validators)){
 		alert('Invalid Email-id Has to have a ‘’@’ and “.” in the email url');
 		return false;
 	}
 
-	if(!mobNumber.value.match(var phonenumber_validators)){
+	if(!mobNumber.value.match(phonenumber_validators)){
 		alert('Invalid Phone Number! Has to be 10 digits, and no alphabets are allowed');
 		return false;
 	}
