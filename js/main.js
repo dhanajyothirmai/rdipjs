1179bf088b2ae46d7f1d19dfb11e31e7083035e2
Jyothirmai Thimmaraju <43008285+dhanajyothirmai@users.noreply.github.com>
2020-09-05 21:13:49 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,15 +3,15 @@
 function checkvalid_form()
 {
 	var form = document.getElementById('form').value;
-	var Name = document.getElementById('name');
-	var Email = document.getElementById('email');
-	var PhoneNumber = document.getElementById('phonenumber');
+	var name = document.getElementById('name');
+	var email = document.getElementById('email');
+	var phonenumber = document.getElementById('phonenumber');
 
 	var name_validators = /^[^0-9][0-9a-zA-Z]+$/;
 	var email_validators = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 	var phonenumber_validators = /^[0-9]\d{9}$/;
 	
-	if(!username.value.match(name_validators)){
+	if(!name.value.match(name_validators)){
 		alert('Invalid Name! Cannot take a number as starting letter');
 		return false;
 	}
@@ -21,7 +21,7 @@ function checkvalid_form()
 		return false;
 	}
 
-	if(!mobNumber.value.match(phonenumber_validators)){
+	if(!phonenumber.value.match(phonenumber_validators)){
 		alert('Invalid Phone Number! Has to be 10 digits, and no alphabets are allowed');
 		return false;
 	}
